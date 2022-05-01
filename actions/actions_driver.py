6c5a027209a77fa1cd7d8732627cb18846745a92
ABTCoder <amalbenson98@gmail.com>
2022-05-01 17:06:02 +0200

    Added intent and action for driver info (wikipedia)

--- actions/actions_driver.py
+++ actions/actions_driver.py
@@ -10,6 +10,7 @@ import json
 from pathlib import Path
 from typing import Any, Text, Dict, List
 import requests
+import wikipedia
 from rasa_sdk.events import SlotSet
 
 from rasa_sdk import Action, Tracker
@@ -56,29 +57,65 @@ class ActionShowDriverStanding(Action):
     def run(self, dispatcher: CollectingDispatcher,tracker: Tracker,domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         driver = next(tracker.get_latest_entity_values('driver'), None)
-        # dispatcher.utter_message(text=driver)
-        # driver = 'leclerc'
-        
-        r=requests.get(url='http://ergast.com/api/f1/current/driverStandings.json')
-
-        if r.status_code == 200 :
-            data = r.json()
-            season = data['MRData']['StandingsTable']['season']
-            ranking = list(data['MRData']['StandingsTable']['StandingsLists'][0]['DriverStandings'])
-            not_found = True
-            for x in ranking:
-                if x['Driver']['driverId'] == driver:
-                    output = x['Driver']['givenName'] + " " + x['Driver']['familyName'] + " "
-                    if x['position'] =='1':
-                        output += "is leading the championship with "+ x['points'] +"\n"
-                    else:
-                        output += "is in position n° "+x['position'] +" with " + x['points'] + " points \n"
-                    not_found = False
-                    break
-            if not_found:
-                output = "Sorry, maybe you spelled his name wrong or he's not participating in the current season\n"
+        if driver is None:
+            driver = tracker.get_slot('driver')
+        if driver is None:
+            output = "Sorry you didn't specify the driver\n"
         else:
-            output = "Sorry there might be a problem with the server, please try again\n"
+        
+            r=requests.get(url='http://ergast.com/api/f1/current/driverStandings.json')
+
+            if r.status_code == 200 :
+                data = r.json()
+                season = data['MRData']['StandingsTable']['season']
+                ranking = list(data['MRData']['StandingsTable']['StandingsLists'][0]['DriverStandings'])
+                not_found = True
+                for x in ranking:
+                    if x['Driver']['driverId'] == driver:
+                        output = x['Driver']['givenName'] + " " + x['Driver']['familyName'] + " "
+                        if x['position'] =='1':
+                            output += "is leading the championship with "+ x['points'] +"\n"
+                        else:
+                            output += "is in position n° "+x['position'] +" with " + x['points'] + " points \n"
+                        not_found = False
+                        break
+                if not_found:
+                    output = "Sorry, maybe you spelled his name wrong or he's not participating in the current season\n"
+            else:
+                output = "Sorry there might be a problem with the server, please try again\n"
+        dispatcher.utter_message(text=output)
+        return []
+
+
+class ActionShowDriverInfo(Action):
+
+    def name(self) -> Text:
+        return "action_show_driver_info"
+
+    def run(self, dispatcher: CollectingDispatcher,tracker: Tracker,domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        driver = next(tracker.get_latest_entity_values('driver'), None)
+        if driver is None:
+            driver = tracker.get_slot('driver')
+        if driver is None:
+            output = "Sorry you didn't specify the driver\n"
+        else:
+            r=requests.get(url='http://ergast.com/api/f1/drivers/'+driver+'.json')
+
+            if r.status_code == 200 :
+                data = r.json()
+                d = data['MRData']['DriverTable']['Drivers'][0]
+                wikipedia.set_lang("en")
+                try:
+                    xt = ""
+                    if d["familyName"] == "Sainz":
+                        xt=" Jr."
+                    summary = wikipedia.summary(d["givenName"]+ " " +d["familyName"]+xt,auto_suggest=False, sentences = 3)
+                    output = str(summary)
+                except Exception as e:
+                    output = e+"There might be problems with wikipedia at the moment, please try later\n"
+            else:
+                output = "Sorry there might be a problem with the server, please try again\n"
         dispatcher.utter_message(text=output)
         return []
      
