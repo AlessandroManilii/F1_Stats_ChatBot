4b18491c553b16beada4daa150c1c309fc974039
AlessandroManilii <alessandromanilii@gmail.com>
2022-05-08 11:51:57 +0200

    Defined new intent "last race qualifying" with related custom action

--- actions/actions_race.py
+++ actions/actions_race.py
@@ -90,78 +90,6 @@ class ActionNextRaceSchedule(Action):
         dispatcher.utter_message(text=output)
         return []
 
-class ActionNthRaceSchedule(Action):
-
-    def name(self) -> Text:
-        return "action_nth_race_schedule"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        
-        nth = {
-            "first": 1,
-            "second": 2,
-            "third": 3,
-            "fourth": 4,
-            "fifth": 5,
-            "sixth": 6,
-            "seventh": 7,
-            "eighth": 8,
-            "nineth": 9,
-            "tenth": 10,
-            "eleventh": 11,
-            "twelth": 12,
-            "thirteenth": 13,
-            "fourteenth": 14,
-            "fifteenth": 15,
-            "sixteenth": 16,
-            "seventeenth": 17,
-            "eighteenth": 18,
-            "nineteenth": 19,
-            "twentieth": 20,
-            "twentieth-first": 21,
-            "twentieth-second": 22,
-            "twentieth-third": 23,
-            "twentieth-fourth": 24,
-            "twentieth-fifth": 25 
-        }
-        
-        #race = next(tracker.get_latest_entity_values('race'), None)
-        #if race is None:
-        race = tracker.get_slot('race')
-        if race is None:
-            output = "Sorry you didn't specify the race.\n"
-        else:
-            if race in nth.keys():
-                race = nth[str(race)]
-            if race not in range(1,25):
-                output = "Sorry you didn't specify a correct race number.\n"
-            r=requests.get(url='http://ergast.com/api/f1/current/'+str(race)+'.json')
-            if r.status_code == 200 :
-                data = r.json()
-                data = data['MRData'] ['RaceTable']['Races'][0]
-                name = data['raceName']
-                schedule_header = "\t\tDate\t\tTime \n"
-                output = "The following it's the official race schedule for " + name + ":\n" \
-                    + schedule_header \
-                    + "Race \t\t" + data['date'] + "\t" + data['time'].replace(':00Z', " (GMT)") + "\n" \
-                    + "First Practice \t" + data['FirstPractice']['date'] \
-                        + "\t" + data['FirstPractice']['time'].replace(':00Z', " (GMT)") + "\n" \
-                    + "Second Practice\t" + data['SecondPractice']['date'] \
-                        + "\t" + data['SecondPractice']['time'].replace(':00Z', " (GMT)") + "\n" \
-                    + "Qualifying \t" +  data['Qualifying']['date'] \
-                        + "\t" + data['Qualifying']['time'].replace(':00Z', " (GMT)") + "\n"
-
-                if 'Sprint' in data:
-                    output += "Sprint \t" +  data['Sprint']['date'] \
-                        + "\t" + data['Sprint']['time'].replace(':00Z', " (GMT)")
-            else:
-                output = "Sorry there might be a problem with the server, please try again\n"
-
-        dispatcher.utter_message(text=output)
-        return []
-
 class ActionLastRace(Action):
 
     def name(self) -> Text:
@@ -237,6 +165,54 @@ class ActionLastRaceResults(Action):
         dispatcher.utter_message(text=output)
         return []
 
+class ActionLastRaceQualifyingInfo(Action):
+
+    def name(self) -> Text:
+        return "action_last_race_qualifying_results"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        r=requests.get(url='http://ergast.com/api/f1/current/last/qualifying.json')
+        if r.status_code == 200 :
+            data = r.json()
+            data = data['MRData'] ['RaceTable']['Races'][0]
+            name = data['raceName']
+            output = "Qualifying results for " + name + " are the following:\n" 
+            results_header = "Grid\tPilot\t\tConstructor\tQ1\t\tQ2\t\tQ3\n"
+            output += results_header
+            results = data['QualifyingResults']
+            for record in results:
+                row = []
+                row.append(record['position'])
+                row.append(record['Driver']['familyName'])
+                row.append(record['Constructor']['name'])
+                if 'Q1' in record:
+                    row.append(record['Q1'])
+                else: 
+                    row.append("---")
+                if 'Q2' in record:
+                    row.append(record['Q2'])
+                else: 
+                    row.append("---")
+                if 'Q3' in record:
+                    row.append(record['Q3'])
+                else: 
+                    row.append("---")
+
+                output += row[0] + "\t"
+                for el in row[1:]:
+                    if len(el)>7:
+                        output += el + "\t" 
+                    else: 
+                        output += el + "\t\t"
+                output +=  "\n"
+        else:
+            output = "Sorry there might be a problem with the server, please try again\n"
+
+        dispatcher.utter_message(text=output)
+        return []
 
 
 class ActionNthRace(Action):
@@ -251,3 +227,78 @@ class ActionNthRace(Action):
         dispatcher.utter_message(text="race 1 stats")
 
         return []
+
+class ActionNthRaceSchedule(Action):
+
+    def name(self) -> Text:
+        return "action_nth_race_schedule"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        nth = {
+            "first": 1,
+            "second": 2,
+            "third": 3,
+            "fourth": 4,
+            "fifth": 5,
+            "sixth": 6,
+            "seventh": 7,
+            "eighth": 8,
+            "nineth": 9,
+            "tenth": 10,
+            "eleventh": 11,
+            "twelth": 12,
+            "thirteenth": 13,
+            "fourteenth": 14,
+            "fifteenth": 15,
+            "sixteenth": 16,
+            "seventeenth": 17,
+            "eighteenth": 18,
+            "nineteenth": 19,
+            "twentieth": 20,
+            "twentieth-first": 21,
+            "twentieth-second": 22,
+            "twentieth-third": 23,
+            "twentieth-fourth": 24,
+            "twentieth-fifth": 25 
+        }
+        
+        race = next(tracker.get_latest_entity_values('race'), None)
+        if race is None:
+            race = tracker.get_slot('race')
+        if race is None:
+            output = "Sorry you didn't specify the race.\n"
+        else:
+            if race in nth.keys():
+                race = nth[str(race)]
+            if race not in range(1,25):
+                output = "Sorry you didn't specify a correct race number.\n"
+            r=requests.get(url='http://ergast.com/api/f1/current/'+str(race)+'.json')
+            if r.status_code == 200 :
+                data = r.json()
+                data = data['MRData'] ['RaceTable']['Races'][0]
+                name = data['raceName']
+                schedule_header = "\t\tDate\t\tTime \n"
+                output = "The following it's the official race schedule for " + name + ":\n" \
+                    + schedule_header \
+                    + "Race \t\t" + data['date'] + "\t" + data['time'].replace(':00Z', " (GMT)") + "\n" \
+                    + "First Practice \t" + data['FirstPractice']['date'] \
+                        + "\t" + data['FirstPractice']['time'].replace(':00Z', " (GMT)") + "\n" \
+                    + "Second Practice\t" + data['SecondPractice']['date'] \
+                        + "\t" + data['SecondPractice']['time'].replace(':00Z', " (GMT)") + "\n" \
+                    + "Qualifying \t" +  data['Qualifying']['date'] \
+                        + "\t" + data['Qualifying']['time'].replace(':00Z', " (GMT)") + "\n"
+
+                if 'Sprint' in data:
+                    output += "Sprint \t" +  data['Sprint']['date'] \
+                        + "\t" + data['Sprint']['time'].replace(':00Z', " (GMT)")
+            else:
+                output = "Sorry there might be a problem with the server, please try again\n"
+
+        dispatcher.utter_message(text=output)
+        return []
+
+
+
