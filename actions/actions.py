5050b971a34ecd1415deb0b003da7362335884cf
BlackeWhite <gian.lu97@hotmail.it>
2022-05-08 19:33:05 +0200

    fix constructors and add tv info

--- actions/actions.py
+++ actions/actions.py
@@ -110,37 +110,42 @@ class ActionConstructorWikipedia(Action):
 
     def run(self, dispatcher: CollectingDispatcher,tracker: Tracker,domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
        
-        team = str(tracker.get_slot('constructor'))
-        wikipedia.set_lang("en")
-        teams = team.replace(" ", "_")
-        teams = string.capwords(teams)
-        #Api ergast per cercare la pagina wikipedia, poichè le scuderie hanno nomi diversi su wikipedia
-        r=requests.get(url='http://ergast.com/api/f1/constructors/'+teams+'.json')
-
-
-        if r.status_code == 200 :
-            data = r.json()
-            wiki_url = data['MRData']['ConstructorTable']['Constructors'][0]['url']
-            split_str = wiki_url.split("/")
-            team = split_str[4]
-
-            if team == "McLaren": #è un bug, se si cerca MClaren esce mc lauren usando il comando dell'else( non so il perchè) 
-                try:
-                    p = wikipedia.WikipediaPage(team)
-                    output = p.summary 
-                except:
-                    output = "I do not know anything about, what a mistery!? Are you sure it is correctly spelled?"
-            else:
-                try:
-                    summary = wikipedia.summary(team, sentences=10)
-                    output = summary
-                except:
-                    output = "I do not know anything about, what a mistery!? Are you sure it is correctly spelled?"    
+        team = next(tracker.get_latest_entity_values('constructor_name'), None)
+        if team is None:
+            team = tracker.get_slot('constructor_name')
+        if team is None:
+            output = "Sorry you didn't specify the team.\n"
         else:
-            output = "I do not know anything about, what a mistery!? Are you sure it is correctly spelled?"
-        
-        dispatcher.utter_message(text=output)
-        return []
+            wikipedia.set_lang("en")
+            teams = team.replace(" ", "_")
+            teams = string.capwords(teams)
+            #Api ergast per cercare la pagina wikipedia, poichè le scuderie hanno nomi diversi su wikipedia
+            r=requests.get(url='http://ergast.com/api/f1/constructors/'+teams+'.json')
+
+
+            if r.status_code == 200 :
+                data = r.json()
+                wiki_url = data['MRData']['ConstructorTable']['Constructors'][0]['url']
+                split_str = wiki_url.split("/")
+                team = split_str[4]
+
+                if team == "McLaren": #è un bug, se si cerca MClaren esce mc lauren usando il comando dell'else( non so il perchè) 
+                    try:
+                        p = wikipedia.WikipediaPage(team)
+                        output = p.summary 
+                    except:
+                        output = "I do not know anything about, what a mistery!? Are you sure it is correctly spelled?"
+                else:
+                    try:
+                        summary = wikipedia.summary(team, sentences=10)
+                        output = summary
+                    except:
+                        output = "I do not know anything about, what a mistery!? Are you sure it is correctly spelled?"    
+            else:
+                output = "I do not know anything about, what a mistery!? Are you sure it is correctly spelled?"
+            
+            dispatcher.utter_message(text=output)
+            return []
 
 class ActionChampionshipWikipedia(Action):
 
